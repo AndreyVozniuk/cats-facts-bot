de371ae9d4e6ea92b9e99ffc0976499c0e7d23eb
Andrew <vozniukandrey@gmail.com>
2020-06-10 22:52:13 +0300

    add helpers object to the main js file

---
+++ src/index.js
@@ -0,0 +1,18 @@
+process.env['NTBA_FIX_319'] = 1 // for work bot without exceptions
+
+const TelegramBot = require('node-telegram-bot-api')
+const fs = require('fs')
+const helpers = require('./helpers')
+const TOKEN = fs.readFileSync('./token.txt', 'utf8')
+
+helpers.botStarted()
+
+const bot = new TelegramBot(TOKEN, { polling: true })
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}, the bot is under development, he will be great, just wait =)`)
+})
+
+bot.onText(/\/help/, (msg) => {
+    bot.sendMessage(msg.chat.id, `For any questions or suggestions you can write to @AndrushaV`)
+})
